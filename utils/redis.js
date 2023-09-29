5d8d19d9eb30f6e116a2dcd7f4ad5aaa32bfe4ff
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-29 21:22:54 +0000

    Created redisClient class in utils

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import promisify from 'util';
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.connected = true;
+    this.client.on('error', (error) => {
+      console.error(error.message);
+      this.connected = false;
+    });
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+module.exports = redisClient;
